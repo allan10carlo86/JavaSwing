852cafdffbeb93984100d88f9cf90f6b1698dd23
Allan Carlo Ramos <allan.carlo.t.ramos@gmail.com>
2018-02-05 23:52:31 +0800

    Commit again BasicSwing.java

--- src/Lessons/BasicSwing.java
+++ src/Lessons/BasicSwing.java
@@ -1,24 +1,32 @@
 package Lessons;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JButton;
+
 public class BasicSwing extends JFrame{
 	
 	JPanel p = new JPanel();
 	JButton b = new JButton("Hello");
+	JTextField t = new JTextField("Hi!");
+	JTextArea ta = new JTextArea("How\nare\nyou?");
 	
 	public static void main(String[] args)
 	{
 		new BasicSwing();
 	}
+	
 	public BasicSwing()
 	{
 		super("Basic Swing App");
 		setSize(400,300);
 		setResizable(true);
 		
-		
 		p.add(b);
+		p.add(t);
+		p.add(ta);
 		add(p);
 		
 		setVisible(true);
